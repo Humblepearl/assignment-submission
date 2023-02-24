25b949430ae1e0bd8ae2922a83411fdd21d87502
humblepearl <125411588+Humblepearl@users.noreply.github.com>
2023-02-24 11:45:35 +0000

    Add files via upload

---
+++ assignment three.py
@@ -0,0 +1,15 @@
+#github\humblepearl
+#script for purchasing a car
+#list of available cars and their prices
+cars={"Corolla": 30000,"Civic":95000,"Ford":35000,"Benz":50000}
+#get user input for the car name
+car_name=input("Enter a car_name")
+#check if the car is in the list available cars if car_name in cars:print("Yes")
+#if car_name=cars[car_name]
+print(f"The price of {car_name} is ${car_price}.")
+else
+    #if car name is not present, inform the user
+    print(f"Sorry,{car_name} is nt available")
+    
+
+
